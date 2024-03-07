a2ab41bb7143a4cc5778ec08fc7e5b927dbecfe6
mcmanus <alex.mcmanus@colorado.edu>
2024-03-06 17:40:53 -0700

    added in S for pdhg/AOI to mri_reconCSPFHomodyne and an option to run avgOpIter with line search with this S. need to look for efficiencies in Sx - x when using pdhg

--- mri_reconCSPFHomodyne.m
+++ mri_reconCSPFHomodyne.m
@@ -1,40 +1,45 @@
-function recon =  mri_reconCSPFHomodyne(kData, sFSR, varargin)
+function recon =  mri_reconCSPFHomodyne(kData, sFSR, varargin )
+  % recon =  mri_reconCSPFHomodyne(kData, sFSR [, 'wavSplit', wavSplit ] )
 
-p = inputParser;
-p.addParameter( 'wavSplit', [], @isnumeric );
-p.parse( varargin{:} );
-wavSplit = p.Results.wavSplit;
+  p = inputParser;
+  p.addParameter( 'alg', 'douglasRachford' );
+  p.addParameter( 'doChecks', false );
+  p.addParameter( 'wavSplit', [], @isnumeric );
+  p.parse( varargin{:} );
+  alg = p.Results.alg;
+  doChecks = p.Results.doChecks;
+  wavSplit = p.Results.wavSplit;
+  
+  sImg = [ size( kData, 1 ) size( kData, 2 ) ];
+  if numel( wavSplit ) == 0
+    wavSplit = makeWavSplit( sImg );
+  end
+  mask = ( kData ~= 0 );
+  sMask = size( mask );
 
-sImg = [ size( kData, 1 ) size( kData, 2 ) ];
-if numel( wavSplit ) == 0
-  wavSplit = makeWavSplit( sImg );
-end
-mask = ( kData ~= 0 );
-n0 = size(mask);
+  unknownMask = 1 - mask;
+  unknownMask( ceil( ( sImg(1) + 1 ) / 2 ) + round( sFSR(1)/2 ) : end, : ) = 0;
+  unknownIndxs = find( unknownMask == 1 );
+  nUnknown = numel( unknownIndxs );
 
-unknownMask = 1 - mask;
-unknownMask( ceil(sImg(1)/2) + round( sFSR(1)/2 ) : end, : ) = 0;
-unknownIndxs = find(unknownMask == 1);
-
-[~,phaseImg] = mri_reconPFHomodyne( kData, sFSR );
-phases = angle( phaseImg );
+  [~,phaseImg] = mri_reconPFHomodyne( kData, sFSR );
+  phases = angle( phaseImg );
 
   function out = applyA(in, op)
     if nargin < 2 || strcmp( op, 'notransp' )
-      x = zeros(n0);
-      x(unknownIndxs) = in;
-      Px = mri_reconPFHomodyne(x, sFSR, 'phases', phases);
-      out = wtDaubechies2(Px, wavSplit);
+      x = zeros(sMask);
+      x( unknownIndxs ) = in;
+      Px = mri_reconPFHomodyne( x, sFSR, 'phases', phases);
+      out = wtDaubechies2( Px, wavSplit );
     else
       Whin = iwtDaubechies2(in, wavSplit);
-      Ptx = mri_reconPFHomodyne(Whin, sFSR, 'phases', phases, 'op', 'transp');
-      out = Ptx(unknownIndxs);
+      Ptx = mri_reconPFHomodyne( Whin, sFSR, 'phases', phases, 'op', 'transp' );
+      out = Ptx( unknownIndxs );
     end
   end
 
+  Ny = sMask(1);
   function out = applyB(in, op)
-
-    Ny = size( in, 1 );
     ys = size2imgCoordinates( Ny );
     centerIndx = find( ys == 0, 1 );
 
@@ -45,131 +50,171 @@ phases = angle( phaseImg );
     ramp( 1 : firstY ) = 0;
     ramp( lastY : end ) = 2;
     ramp = 2 - ramp;
-    ramp2 = ramp.*ramp;
+    ramp2 = ramp .* ramp;
     ramp3 = bsxfun(@times, unknownMask, ramp2);
-    alpha = 1/8;
-    nramp = size(ramp);
-    bramp = (1/alpha) *ones(nramp) - ramp3;
-    sbramp = sqrt(bramp);
+    rampB = sqrt( numel( mask ) * ones( sMask ) - ramp3 );
+    % This scaling inside the square root makes A A^T + B B^T = I
 
+    in = reshape( in, sMask );
     if nargin < 2 || strcmp( op, 'notransp' )
-      Py = mri_reconPFHomodyne(in, sFSR, 'phases', phases, 'ramp', sbramp);
+      Py = mri_reconPFHomodyne(in, sFSR, 'phases', phases, 'ramp', rampB);
       out = wtDaubechies2(Py, wavSplit);
     else
       WhIn = iwtDaubechies2(in, wavSplit);
-      out = mri_reconPFHomodyne(WhIn, sFSR, 'phases', phases, 'ramp', sbramp, 'op', 'transp');
+      out = mri_reconPFHomodyne(WhIn, sFSR, 'phases', phases, 'ramp', rampB, 'op', 'transp');
     end
   end
 
-  function out = proxftilde( in, t )   %#ok<INUSD>
-    out = zeros(size(in));
-    out(1:n) = in(1:n);
+  nMask = prod( sMask );
+  function out = applyAB( in, op )
+    if nargin < 2 || strcmp( op, 'notransp' )
+      out = applyA( in(1:nUnknown) ) + applyB( in(nUnknown+1:end) );
+    else
+      out = zeros( nUnknown + nMask, 1 );
+      out(1:nUnknown) = applyA( in, 'transp' );
+      out(nUnknown+1:end) = applyB( in, 'transp' );
+    end
   end
 
-  function out = proxL1(in, sigma, b)
-    out = softThresh(in + b, sigma) - b;
+  if doChecks == true
+    innerProd = @(x,y) real( dotP( x, y ) );
+
+    x0 = rand( numel( unknownIndxs ), 1 );
+    [chkA, errChkA] = checkAdjoint( x0, @applyA, 'innerProd', innerProd );
+    if chkA == false
+      error(['Check for adjoint of A failed with error ', num2str(errChkA) ]);
+    else
+      disp( 'Check for adjoint of A passed.' );
+    end
+
+    y0 = rand( sMask );
+    [chkB, errChkB] = checkAdjoint( y0, @applyB, 'innerProd', innerProd );
+    if chkB == false
+      error(['Check for adjoint of B failed with error ', num2str(errChkB) ]);
+    else
+      disp( 'Check for adjoint of B passed.' );
+    end
+
+    % Check to make sure A A^T + B B^T is a scaled identity
+    AATpBBTy = applyA( applyA( y0, 'transp' ) ) + applyB( applyB( y0, 'transp' ) );
+    testAB = AATpBBTy ./ y0;
+    errAB = std( testAB(:) );
+    if errAB > 1d-12
+      error([ 'AB check failed with error ', num2str(errAB) ]);
+    else
+      disp( 'AB check passed' );
+    end
   end
 
-  PsiPb = wtDaubechies2( mri_reconPFHomodyne( kData, sFSR, 'phases', phases ), wavSplit ) ;
-  function out = proxgconj( in, sigma )
-    out = in - sigma * proxL1( in/sigma, 1/sigma, PsiPb );
+  function out = f_tilde( in )
+    if max( abs( in( nUnknown + 1 : end ) ) ) > 0
+      out = Inf;
+    else
+      out = 0;
+    end
   end
 
-  function out = g(in)
-    out = norm(in(:) + PsiPb(:), 1);
+  function out = proxf_tilde( in, t )   %#ok<INUSD>
+    out = zeros( size( in ) );
+    out( 1 : nUnknown ) = in( 1 : nUnknown );
   end
 
-  function out = gtilde(in)
-    x = in(1:n);
-    y = in(n+1:end);
-    y = reshape(y, n0);
-    out = g( applyA(x) + applyB(y) );
+  PsiPMcTb = wtDaubechies2( mri_reconPFHomodyne( kData, sFSR, 'phases', phases ), wavSplit ) ;
+  function out = g( in )
+    out = norm( in(:) + PsiPMcTb(:), 1 );
   end
 
-
-  function out = ftilde(in)
-%     y = in(n+1:end);
-%     if norm(y, 'inf') > 0
-%       out = Inf;
-%     else
-%       out = 0;
-%     end
-    out = 0;
+  function out = proxg( in, t )
+    out = softThresh( in + PsiPMcTb, t ) - PsiPMcTb;
   end
 
-  function out = proxgtilde(in, tau)
-    %%% slide 12.8 Vandenberghe's notes 236C
-    sigma = theta / tau;
-    x = in(1:n);
-    y = in(n+1:end);
-    y = reshape(y, n0);
-    t = proxgconj( sigma * ( applyA(x) + applyB(y) ), sigma);
-    Att = applyA( t, 'transp' );
-    Btt = applyB( t, 'transp' );
-    out = in - tau * [Att; Btt(:)];
+    function out = proxconj_g( in, t )
+        out = proxConj( @proxg, in, t );
+    end
+
+  function out = g_tilde( in )
+    x = in( 1 : nUnknown );
+    y = reshape( in( nUnknown + 1 : end ), sMask );
+    AxBy = applyA( x ) + applyB( y );
+    out = norm( AxBy(:) + PsiPMcTb(:), 1 );
   end
 
-  function out = proxgtildeconj( in, lambda )
-    out = in - lambda*proxgtilde(in/lambda, 1/lambda);
+  function out = proxg_tilde( in, t )
+    out = proxCompositionAffine( @proxg, in, @applyAB, 0, 1, t );
   end
 
-  function out = proxf( in, t )
-    out = in;
+  function out = proxg_tildeConj( in, t )
+    out = proxConj( @proxg_tilde, in, t );
   end
 
-  function out = f(in)
-    out = 0;
+  function out = Rf( in, gamma )
+    out = 2 * proxf_tilde( in, gamma ) - in;
   end
 
-testx = randn(size(kData));
-ATk = applyA(testx, 'transp');
-AATk = applyA(ATk);
+  function out = Rg( in, gamma )
+    out = 2 * proxg_tilde( in, gamma ) - in;
+  end
 
-BTk = applyB(testx, 'transp');
-BBTk = applyB(BTk);
+  gamma = 1d-3;
+  function out = S_DR( in )
+    out = Rg( Rf( in, gamma ), gamma );
+  end
 
-test1 = AATk + BBTk;
-testratio = test1 ./ testx;
+  function out = Rg_tildeConj( in, gamma )
+    out = 2 * proxg_tildeConj( in, gamma ) - in;
+  end
 
+  function out = S_pdDR( in )
+    out = -gamma * Rg_tildeConj( Rf( in, gamma ) / gamma , 1/gamma );
+  end
 
-img0 = fftshift2( ifft2( ifftshift2( kData ) ) );
-x0 = wtDaubechies2( img0, wavSplit );
-value = findValueBelowFraction( x0, 0.9 );
-x0( x0 < value ) = 0;
-img0 = iwtDaubechies2( x0, wavSplit );
-fftImg0 = fftshift2( fft2( ifftshift2( img0 ) ) );
-k0 = fftImg0( unknownIndxs );
-n = numel(k0);
-z0 = zeros(n0);
+    function out = S_pdhg( in )
+        t1 = Rf( in, gamma );
+        t2 = applyAB(t1);
+        t3 = proxconj_g(t2/gamma, 1/gamma);
 
-theta = 8/prod(sImg);
-k0 = randn(size(k0)) + 1i*randn(size(k0));
-x0 = [k0; z0(:)];
-maxIter = 10;
+        out = t1 - 2*gamma*applyAB(t3, 'transp');
 
-normA = powerIteration(@applyA, rand( numel( unknownIndxs ), 1 ) );
-tau = 0.95 / normA;
-sigma_pdhg = 0.95 / normA;
+    end
 
-% [xStar,objValues,relDiffs] = pdhg( k0, @proxf, @proxgconj, tau, ...
-%     'sigma', sigma_pdhg, 'A', @applyA, 'f', @f, 'g', @g, 'N', 1000, 'normA', normA, 'printEvery', 50, ...
-%     'verbose', true, 'tol', [] );
+  k0 = zeros( nUnknown, 1 );
+  z0 = zeros( sMask );
+  x0 = [ k0; z0(:) ];
 
-[xStar_drwls, iters1, alphas1] = dr_wLS(x0, @proxftilde, @proxgtilde, maxIter);
-% xStar1 = douglasRachford(x0, @proxftilde, @proxgtilde, 1, 'verbose', true);
-[xStar2, iters2] = primal_dual_dr_2(x0, @proxftilde, @proxgtildeconj);
-[xStar3, iters3] = primal_dual_dr_aoi(x0, @proxftilde, @proxgtildeconj);
-[xStar, iters, alphas, objVals] = primal_dual_dr_aoi_wls(x0, @proxftilde, @proxgtildeconj, @ftilde, @gtilde, maxIter);
+  switch alg
 
-out = kData;
-out(unknownIndxs) = xStar(1:n);
+    case 'douglasRachford'
+      [xStar,objValues] = douglasRachford( x0, @proxf_tilde, @proxg_tilde, 1d-3, 'N', 200, ...
+        'verbose', true, 'printEvery', 10, 'f', @f_tilde, 'g', @g_tilde);   %#ok<ASGLU>
 
-% out1 = kData;
-% out1(unknownIndxs) = xStar1(1:n);
-% 
-% out2 = kData;
-% out2(unknownIndxs) = xStar2(1:n);
+    case 'douglasRachford_avgOp'
+      objF = @(x) f( proxf_tilde(x) ) + g( proxf_tilde(x) );
+      [xStar] = avgOpIter( x0, @S_DR, 'alpha', 0.5, 'N', 1000, ...
+        'verbose', true, 'objFunction', objF );
 
-recon = mri_reconPFHomodyne(out, sFSR, 'phases', phases);
+    case 'primalDualDR'
+      [xStar, objValues] = primalDualDR( x0, @proxf_tilde, @proxg_tildeConj, 1d-3, 'N', 1000, ...
+        'verbose', true, 'f', @f_tilde, 'g', @g_tilde );   %#ok<ASGLU>
 
+    case 'primalDualDR_avgOp'
+      [xStar] = avgOpIter( x0, @S_pdDR, 'alpha', 0.5, 'N', 1000, ...
+        'verbose', true, 'objFunction', @g_tilde );
+
+      case 'primalDualDR_avgOp_wls'
+      objF = @(x) f_tilde( proxf_tilde(x) ) + g_tilde( proxf_tilde(x) );
+      [xStar,objValues,alphas] = avgOpIter_wLS( x0, @S_pdDR, 'N', 100, ...
+        'verbose', true, 'printEvery', 1, 'objFunction', objF );
+
+      case 'pdhg_avgOp_wls'
+      objF = @(x) f_tilde( proxf_tilde(x) ) + g_tilde( proxf_tilde(x) );
+      [xStar,objValues,alphas] = avgOpIter_wLS( x0, @S_pdhg, 'N', 100, ...
+        'verbose', true, 'printEvery', 1, 'objFunction', objF );
+
+    otherwise
+      error( 'Unrecognized algorithm' );
+  end
+
+  kOut = kData;
+  kOut( unknownIndxs ) = xStar( 1 : nUnknown );
+  recon = mri_reconPFHomodyne( kOut, sFSR, 'phases', phases);
 end
--- script_runCSPF_PDHG_linesearch.m
+++ script_runCSPF_PDHG_linesearch.m
@@ -49,8 +49,9 @@ for coilIdx = 1:8
   % cspfRecons{1,1,coilIdx} = mri_reconCSPFWithPDHG( fftSamples_wavACR_pf, sFSR, 'wavSplit', wavSplit  );
 %   linesearchRecons{1,1,coilIdx} = mri_reconCSPFWithPDHG_WLS( fftSamples_wavACR_pf, sFSR, 'wavSplit', wavSplit  );
   %t1 = mri_reconCSPFHomodyne( fftSamples_wavACR_pf, sFSR, 'wavSplit', wavSplit  );
-  t2 = mri_reconCSPFHomodyne_nick( fftSamples_wavACR_pf, sFSR, 'wavSplit', wavSplit, 'alg', 'primalDualDR_avgOp'  );
-  t3 = mri_reconCSPFHomodyne_nick( fftSamples_wavACR_pf, sFSR, 'wavSplit', wavSplit, 'alg', 'primalDualDR_avgOp_wls'  );
+  t1 = mri_reconCSPFHomodyne_alex_pdhg( fftSamples_wavACR_pf, sFSR, 'wavSplit', wavSplit, 'alg', 'primalDualDR_avgOp_wls'  );
+  t2 = mri_reconCSPFHomodyne_alex_pdhg( fftSamples_wavACR_pf, sFSR, 'wavSplit', wavSplit, 'alg', 'pdhg_avgOp_wls'  );
+  
 end
 % csRecons = cell2mat( csRecons );  csRecon = mri_reconRoemer( csRecons );
 % pfRecons = cell2mat( pfRecons );  pfRecon = mri_reconRoemer( pfRecons );
